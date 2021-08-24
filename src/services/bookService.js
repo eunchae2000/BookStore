35bfc0b43aa0b2b589958b5597b0ba9896932156
coco <tldjs3651@naver.com>
2021-08-24 15:49:22 +0900

    services 수정,,

--- src/services/bookService.js
+++ src/services/bookService.js
@@ -11,19 +11,28 @@ exports.main = async(req, res, next) =>{
     }
 }
 
-exports.signUp = async(req, res, next) =>{
+exports.signUp = async(user_uid, user_password, user_name) =>{
     try{
-        let signup = await pool.query(bookQuery.signUp);
+        let signup = await pool.query(bookQuery.signUp, [user_uid, user_password, user_name]);
         return signup[0]
     }catch(err){
         throw Error(err)
     }
 } 
 
-exports.logIn = async(req, res, next) => {
+exports.signIn = async(user_uid, user_password) => {
     try{
-        let login = await pool.query(bookQuery.logIn)
+        let login = await pool.query(bookQuery.signIn, [user_uid, user_password])
         return login[0]
+    }catch(err){
+        throw Error(err);
+    }
+}
+
+exports.mypage = async(req, res, next) =>{
+    try{
+        let userInfo = await ppol.query(bookQuery.mypage);
+        return userInfo[0]
     }catch(err){
         throw Error(err);
     }
