cdb34351ae60962e56029829c589ade6bf36cddf
coco <tldjs3651@naver.com>
2021-08-26 23:05:14 +0900

    데이터베이스 정보 수정

--- middleware/bookData.js
+++ middleware/bookData.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise')
 
-const pool = mysql.createConnection({
+const pool = mysql.createPool({
     host:'localhost',
     user: 'root',
-    password: '01090442332',
-    port: 3306,
-    database: 'bookStoreDB'
+    password: '1234',
+    port: 3300,
+    database: 'mydb'
 })
 
 module.exports = pool;
